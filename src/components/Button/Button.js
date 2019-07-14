ccebcd7f9ce806335e58c8e4fd144e27ac627c70
Neology92 <oskarlegner@gmail.com>
2019-07-14 07:14:50 +0200

    Create button template

---
+++ src/components/Button/Button.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  height: ${({ isBig }) => (isBig ? '36px' : '33px')};
+  padding: 10px 14px;
+
+  background: ${({ isTransparent, theme }) =>
+    isTransparent ? 'none' : theme.color.lightBlue};
+
+  border: ${({ isTransparent, theme }) =>
+    isTransparent ? `1px solid ${theme.color.darkBlue}` : 'none'};
+  border-radius: 10px;
+
+  color: ${({ isTransparent, theme }) =>
+    isTransparent ? theme.color.darkBlue : theme.color.white};
+  font-family: ${({ theme }) => theme.font.family.roboto};
+  font-weight: 500;
+  font-size: ${({ isBig }) => (isBig ? '18px' : '14px')};
+  line-height: 50%;
+`;
+
+export default Button;
