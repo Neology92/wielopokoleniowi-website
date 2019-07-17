8ec49c6b04a64549e9b5576864dc12f8cde0f4db
Neology92 <oskarlegner@gmail.com>
2019-07-17 10:48:21 +0200

    Make articles grid responsive

--- src/components/ArticleTile.js
+++ src/components/ArticleTile.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const TileWrapper = styled.div`
-  height: 190px;
   width: 165px;
+  height: 190px;
   border-radius: 10px;
   background: ${({ theme }) => theme.color.grey};
+
+  ${({ theme }) => theme.media.above.m} {
+    width: 165px;
+    height: 190px;
+  }
+
+  ${({ theme }) => theme.media.above.l} {
+    width: 327px;
+    height: 328px;
+  }
+
+  ${({ theme }) => theme.media.above.xl} {
+    width: 327px;
+    height: 328px;
+  }
 `;
 
 // eslint-disable-next-line
--- src/components/ArticlesGrid.js
+++ src/components/ArticlesGrid.js
@@ -4,10 +4,36 @@ import styled from 'styled-components';
 import ArticleTile from './ArticleTile';
 
 const ArtcilesWrapper = styled.div`
+  margin: 24px auto 0;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(2, 165px);
   grid-template-rows: repeat(3, 1fr);
   grid-gap: 10px 10px;
+  justify-content: center;
+
+  ${({ theme }) => theme.media.above.s} {
+    grid-template-columns: repeat(2, 165px);
+    grid-template-rows: repeat(3, 1fr);
+    grid-gap: 30px 36px;
+  }
+
+  ${({ theme }) => theme.media.above.m} {
+    grid-template-columns: repeat(3, 165px);
+    grid-template-rows: repeat(2, 1fr);
+    grid-gap: 30px 36px;
+  }
+
+  ${({ theme }) => theme.media.above.l} {
+    grid-template-columns: repeat(2, 327px);
+    grid-template-rows: repeat(3, 1fr);
+    grid-gap: 30px 36px;
+  }
+
+  ${({ theme }) => theme.media.above.xl} {
+    grid-template-columns: repeat(3, 327px);
+    grid-template-rows: repeat(2, 1fr);
+    grid-gap: 30px 36px;
+  }
 `;
 
 // eslint-disable-next-line
