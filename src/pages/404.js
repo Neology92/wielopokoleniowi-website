e0f77581a284d57722b669c4228d73825b38f49e
Neology92 <oskarlegner@gmail.com>
2019-07-21 18:34:02 +0200

    Add some pages

---
+++ src/pages/404.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { MainLayout } from 'layouts';
+import { SEO, Menu } from 'components';
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 80vh;
+  margin: 0 0 10vw 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    font-size: 4rem;
+    margin: 0 auto;
+  }
+`;
+
+const PageNotFound = () => (
+  <MainLayout>
+    <SEO title="Home" />
+    <Menu />
+    <StyledContainer>
+      <h1>404</h1>
+      <h1>Page not found</h1>
+    </StyledContainer>
+  </MainLayout>
+);
+
+export default PageNotFound;
---
+++ src/pages/regulamin.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { MainLayout } from 'layouts';
+import { SEO, Menu } from 'components';
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 80vh;
+  margin: 0 0 10vw 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    font-size: 4rem;
+    margin: 0 auto;
+  }
+`;
+
+const About = () => (
+  <MainLayout>
+    <SEO title="Home" />
+    <Menu />
+    <StyledContainer>
+      <h1>Regulamin</h1>
+    </StyledContainer>
+  </MainLayout>
+);
+
+export default About;
