2f2b6198b2ffe159307872078c52db970eaff26a
Neology92 <oskarlegner@gmail.com>
2019-07-21 18:33:43 +0200

    Fix propTypes validation

--- src/components/PostTile/PostTile.js
+++ src/components/PostTile/PostTile.js
@@ -20,7 +20,18 @@ const PostTile = ({ post }) => {
 };
 
 PostTile.propTypes = {
-  post: PropTypes.instanceOf(Array).isRequired,
+  post: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    body: PropTypes.shape({
+      html: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+    }).isRequired,
+    icon: PropTypes.shape({
+      url: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default PostTile;
