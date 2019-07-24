146ed51d78a33866b8984e5de71dfa00484068df
Neology92 <oskarlegner@gmail.com>
2019-07-24 16:06:18 +0200

    Add new pages

---
+++ src/pages/artykuly/dla-kazdego.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { MainLayout } from 'layouts';
+import { SEO, Menu } from 'components';
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 80vh;
+  margin: 0 0 10vw 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    font-size: 4rem;
+  }
+
+  h1,
+  h2 {
+    margin: 0 auto;
+  }
+`;
+
+const Artykuly = () => (
+  <MainLayout>
+    <SEO title="Home" />
+    <Menu />
+    <StyledContainer>
+      <h1>Artykuły</h1>
+      <h2>Dla Każdego</h2>
+    </StyledContainer>
+  </MainLayout>
+);
+
+export default Artykuly;
