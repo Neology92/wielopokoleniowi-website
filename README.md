43ace8373e690e479b320974c8bced72f1229a86
Neology92 <oskarlegner@gmail.com>
2019-08-05 20:21:20 +0200

    Make artykuly subpage redirecting to dla-kazdego

--- README.md
+++ README.md
@@ -2,13 +2,10 @@ TODO:
 
 - repair clickOutside menu categories
 - repair dummy way of setting graphics in footer
+- Protected connection to API....
 - stars legend
 - remove white stripe in topbar (just few resolutions)
-
 - make loading from up to down
-
 - Make lazyload animation more efficient
-
-- Protected connection to API....
-
+- create 'artykuly' subpage instead redirect
 - ARIA
--- src/pages/artykuly.js
+++ src/pages/artykuly.js
@@ -1,50 +1,39 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import { useEffect } from 'react';
+import { navigate } from 'gatsby';
 
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-import { MainLayout } from 'layouts';
-import { SEO, Breadcrumbs } from 'components';
+// import { MainLayout } from 'layouts';
+// import { SEO, Breadcrumbs } from 'components';
 
-const StyledContainer = styled.div`
-  width: 100%;
-  height: 80vh;
-  margin: 0 0 10vw 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
+// const StyledContainer = styled.div`
+//   width: 100%;
+//   height: 80vh;
+//   margin: 0 0 10vw 0;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: flex-start;
 
-  h1 {
-    font-size: 4rem;
-  }
+//   h1 {
+//     font-size: 4rem;
+//   }
 
-  h1,
-  h2 {
-    margin: 0 auto;
-  }
-`;
+//   h1,
+//   h2 {
+//     margin: 0 auto;
+//   }
+// `;
 
-const BreadcrumbsWrapper = styled.div`
-  padding: 15px 0 0 15px;
-  width: 100%;
-`;
+// const BreadcrumbsWrapper = styled.div`
+//   padding: 15px 0 0 15px;
+//   width: 100%;
+// `;
 
-const Artykuly = () => (
-  <MainLayout>
-    <SEO title="Artykuły" />
-    <BreadcrumbsWrapper>
-      <Breadcrumbs />
-    </BreadcrumbsWrapper>
-    <StyledContainer>
-      <h1>Artykuły</h1>
-      <h2>
-        <Link to="/artykuly/dla-biznesu">Dla Biznesu</Link>
-      </h2>
-      <h2>
-        <Link to="/artykuly/dla-kazdego">Dla Każdego</Link>
-      </h2>
-    </StyledContainer>
-  </MainLayout>
-);
+const Artykuly = () => {
+  useEffect(() => {
+    navigate('/artykuly/dla-kazdego');
+  }, []);
+  return null;
+};
 
 export default Artykuly;
