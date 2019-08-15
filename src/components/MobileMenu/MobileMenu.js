d7f4d851c15ce9a3948075923df12ae1bae7da3c
Neology92 <oskarlegner@gmail.com>
2019-08-15 15:55:46 +0200

    New mobile menu items

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import slugify from 'slugify';
-
 import MenuLink from './MenuLink';
 import MenuWrapper from './MenuWrapper';
 import IconsWrapper from './IconsWrapper';
 import MenuLinksWrapper from './MenuLinksWrapper';
 import { StyledLink, StyledFacebook, StyledYoutube } from './styled';
 
-const menuItems = ['O autorze', 'Artykuły', 'Kontakt', 'Newsletter'];
+const menuItems = [
+  { name: 'O autorze', slug: '/o-autorze' },
+  { name: '', slug: '' },
+  { name: 'Dla Każdego', slug: '/artykuly/dla-kazdego' },
+  { name: 'Dla Biznesu', slug: '/artykuly/dla-biznesu' },
+  { name: '', slug: '' },
+  { name: 'Kontakt', slug: '/kontakt' },
+  { name: 'Newsletter', slug: '/newsletter' },
+];
 
 const MobileMenu = ({ className, isOpen }) => (
   <MenuWrapper className={className} isOpen={isOpen}>
     <MenuLinksWrapper>
       {menuItems.map(item => (
-        <MenuLink isOpen={isOpen} key={item}>
-          <StyledLink to={`/${slugify(item.toLowerCase())}`}>{item}</StyledLink>
+        <MenuLink isOpen={isOpen} key={item.name}>
+          <StyledLink to={item.slug}>{item.name}</StyledLink>
         </MenuLink>
       ))}
     </MenuLinksWrapper>
